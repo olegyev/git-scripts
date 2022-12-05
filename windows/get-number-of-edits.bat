3119e03ad83dd6247d9461663b3206500110e734
Aleh Yeustratsyeu <Aleh.Yeustratsyeu@itechart-group.com>
2022-12-05 14:43:52 +0100

    Add sorting to get-number-of-edits

--- windows/get-number-of-edits.bat
+++ windows/get-number-of-edits.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-rem NB! Place this script into a root folder of your project, which is under GIT control
+rem // NB! Place this script into a root folder of your project, which is under GIT control
 
-rem Checkout the master branch
+rem // Checkout the master branch
 git checkout master
 
-rem Get a list of all the files in the repository
+rem // Get a list of all the files in the repository
 set files=
 
 set i=0
@@ -16,21 +16,35 @@ for /f "tokens=*" %%f in ('git ls-tree --full-tree -r --name-only HEAD') do (
 )
 set last_index=%i%
 
-rem Calculate a number of edits (additions and deletions) for each file
+rem // Calculate a number of edits (additions and deletions) for each file
 set lines=
 
-rem A git command returns lines like '3       1       .gitignore'
-for /L %%i in (1,1,%last_index%) do (
+rem // A git command returns lines like '3       1       .gitignore'
+for /l %%i in (1,1,%last_index%) do (
   set edits=
   for /f "tokens=1, 2" %%a in ('git log --numstat --pretty^="format:" !files[%%i]!') do (
     set /a edits[%%i]+=%%a + %%b
   )
-  set lines[%%i]=!edits[%%i]! 
+  set lines[%%i]=!edits[%%i]! !files[%%i]! 
 )
 
-rem Write array into a file
-for /L %%i in (1,1,%last_index%) do (
+rem // Write array into a file
+for /l %%i in (1,1,%last_index%) do (
   @echo !lines[%%i]! >> number_of_edits.txt
 )
 
-sort /R tmp3.txt
+set "file=number_of_edits.txt"
+>"%file%.new1" (
+  for /f "usebackq tokens=1* delims= " %%A in ("%file%") do (
+    set "n=000000000000000%%A"
+    set "str=%%B"
+    setlocal enableDelayedExpansion
+    echo !n:~-15! - !str!
+    endlocal
+  )
+)
+>"%file%.new2" sort /r "%file%.new1"
+>"%file%" (
+  for /f "usebackq tokens=* delims=0" %%A in ("%file%.new2") do echo %%A
+)
+del "%file%.new?"
